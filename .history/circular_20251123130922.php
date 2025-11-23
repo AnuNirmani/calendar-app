c6a5f567a649c2e33a35b5690a00f826ab090d04
pmsdiwyanjalee <pmsdiwyanjalee@students.nsbm.ac.lk>
2025-11-23 13:09:59 +0530

    create circulars

--- .history/circular_20251123130922.php
+++ .history/circular_20251123130922.php
@@ -399,3 +399,4 @@ if (!$result) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+this is circular.php code , can you make this page more attractive and give me the full code please. change the color as navy blue and comfortable to see.
